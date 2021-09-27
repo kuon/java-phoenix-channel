e65cfc392f7f88f4443eb7c6c9428d213b827db4
Nicolas goy <kuon@goyman.com>
2021-09-27 06:48:31 +0200

    Update mock server

--- src/test/mock_server/mix.exs
+++ src/test/mock_server/mix.exs
@@ -32,8 +32,8 @@ defmodule MockServer.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.11"},
-      {:phoenix_pubsub, "~> 1.1"},
+      {:phoenix, "~> 1.6.0"},
+      {:phoenix_pubsub, "~> 2.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"}
